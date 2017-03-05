fa9aa7b454d721a7f750d2b1d899f5ee2c86d33d
esumerfd <esumerfd@bitbashers.org>
2017-03-05 15:56:28 -0500

    add more tests for formattig hands

--- lib/hand.ex
+++ lib/hand.ex
@@ -1,9 +1,4 @@
 defmodule Hand do
-  #def parse(hand) do
-    #Regex.scan(~r/../, hand)
-    #|> Card.parse
-  #end
-
   def format(hand) do
     Enum.map(hand, fn(card) -> Card.format(card) end)
     |> Enum.join
--- test/hand_test.exs
+++ test/hand_test.exs
@@ -2,14 +2,22 @@ defmodule HandTest do
   use ExUnit.Case
   doctest Hand
 
-  #test "card is two of hearts" do
-    #assert [ [Card.twoHearts] ] == Hand.parse("2H")
-  #end
-
   test "format hand as string" do
     hand = [ [:heart, :two], [:spade, :ace], [:club, :king], [:club, :two], [:diamond, :ten] ]
 
     assert "2HASKC2CTD" == Hand.format(hand)
+  end
+
+  test "format any number of cards" do
+    hand = [ [:heart, :two], [:spade, :ace] ]
+
+    assert "2HAS" == Hand.format(hand)
+  end
+
+  test "format empty string if there are no cards" do
+    hand = []
+
+    assert "" == Hand.format(hand)
   end
 end
 
