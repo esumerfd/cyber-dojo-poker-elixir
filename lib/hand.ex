16e20680284f201720d2380f2b1a1e05f0a31abd
Edward Sumerfield <esumerfield@C02Q7KGZG8WP.local>
2017-03-16 21:26:22 -0400

    identify three of a kind

--- lib/hand.ex
+++ lib/hand.ex
@@ -16,29 +16,40 @@ defmodule Hand do
   #
   # Hande identitication
   #
+  #  Input:  [[:heart, :seven], [:club, :seven], [:diamond, :two], [:spade, :seven], [:diamond, :three]]
+  #
 
   def is_high_card(_) do
     true
   end
 
+  @doc """
+    Pair: 2 of the 5 cards in the hand have the same value. 
+  """
   def is_pair(hand) do
     Enum.group_by(hand, fn [_, face] -> face end)
     |> Enum.find(fn {_, cards} -> length(cards) == 2  end)
   end
 
+  @doc """
+    Two Pairs: The hand contains 2 different pairs. 
+  """
   def is_two_pair(hand) do
     2 == Enum.group_by(hand, fn [_, face] -> face end)
     |> Enum.filter(fn {_, cards} -> length(cards) == 2  end)
     |> length
   end
 
-  def is_three_of_a_kind(_) do
-    true
+  @doc """
+    Three of a Kind: Three of the cards in the hand have the same value. 
+  """
+  def is_three_of_a_kind(hand) do
+    Enum.group_by(hand, fn [_, face] -> face end)
+    |> Enum.filter(fn {_, cards} -> length(cards) == 3  end)
   end
 
   @doc """
-    Each face is a consecutive sequence.
-    Input:  [[:heart, :seven], [:club, :seven], [:diamond, :two], [:spade, :seven], [:diamond, :three]]
+    Straight: Hand contains 5 cards with consecutive values.
   """
   def is_straight(hand) do
     {low, high} = low_hi_card(hand)
@@ -50,21 +61,29 @@ defmodule Hand do
   end
 
   @doc """
-    All cards must be of the same suit
-    Input:  [[:heart, :seven], [:heart, :jack], [:heart, :two], [:heart, :eight], [:heart, :three]]
+    Flush: Hand contains 5 cards of the same suit.
   """
   def is_flush(hand) do
     1 == Enum.group_by(hand, fn [suit, _] -> suit end) |> Map.size
   end
 
+  @doc """
+    Full House: 3 cards of the same value, with the remaining 2 cards forming a pair. 
+  """
   def is_full_house(_) do
     true
   end
 
+  @doc """
+    Four of a kind: 4 cards with the same value.
+  """
   def is_four_of_a_kind(_) do
     true
   end
 
+  @doc """
+    Straight flush: 5 cards of the same suit with consecutive values.
+  """
   def is_straight_flush(_) do
     true
   end
